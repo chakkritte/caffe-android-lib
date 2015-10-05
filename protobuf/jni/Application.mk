6b682bbbf82c3d39a5e668ebf18f8630846375ff
Chakkrit Termritthikun <chakkritte57@nu.ac.th>
2015-10-06 00:15:17 +0700

    Update Application.mk

--- protobuf/jni/Application.mk
+++ protobuf/jni/Application.mk
@@ -1,5 +1,5 @@
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_PLATFORM := android-21
 APP_STL := gnustl_static
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
 # APP_MODULE := libprotobuf
